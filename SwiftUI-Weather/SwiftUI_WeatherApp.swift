b7497be0578c84d73dc1dec4858dc9c7a379fce8
tinajuresko <tina.juresko16@gmail.com>
2024-09-25 16:31:26 +0200

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Tina Jureško on 25.09.2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-WeatherTests/SwiftUI_WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherTests.swift
+//  SwiftUI-WeatherTests
+//
+//  Created by Tina Jureško on 25.09.2024..
+//
+
+import Testing
+@testable import SwiftUI_Weather
+
+struct SwiftUI_WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
