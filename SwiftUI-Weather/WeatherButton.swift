7ef1be49efd689a59228a085668980f5eee232a2
tinajuresko <tina.juresko16@gmail.com>
2024-09-26 10:56:32 +0200

    Commit 2

--- SwiftUI-Weather/WeatherButton.swift
+++ SwiftUI-Weather/WeatherButton.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundStyle(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
+
